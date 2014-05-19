ea9617a8d3892d9dc59f422978658e476ce99119
Cédric Perriard <cedric.perriard@pensionsmyndigehten.se>
2014-05-19 13:22:31 +0200

    preparing and restoring

--- R/show-diff.R
+++ R/show-diff.R
@@ -1,21 +1,105 @@
 #' Create a diff with git and export to html
 #' 
-#' @param file
-#' @param revision
-#' @param ins 
-#' @param del
-#' @param git.options
-#' @param clean
-#' @param output
+#' @param file file to create diff for
+#' @param revision one or two revisions of the git repository. If one revision is provided, the diff will be between the revision (old, A) and your local file (new, B). If there are two revisions, the diff is between the first revision (old, A) and the second revision (new, B). 
+#' @param context How mutch context should the output contain? \code{full} for the whole file, \code{auto} for git standard (3 lines), or any number used in the \code{git diff option --unified=context} 
+#' @param git.options string, contains options passed to the git command. The following options are already set and cannot be overwritten: \code{--color=always}, \code{--color-words}, and \code{--word-diff}, \code{--unified=context}
+#' @param output 
+#' @param output.file
 #' 
+#' @export
 #' 
 show_diff <- function (
   file, 
   revision = NULL, 
-  git.options = "--color-words --word-diff --ignore-space-change --ignore-blank-lines --minimal", 
+  context = c("full", "auto")
+  git.options = "--ignore-space-change --ignore-blank-lines --minimal", 
   clean = TRUE, 
-  output = c("viewer", "return")) {
+  output = c("viewer", "return", "file"), 
+  output.file = NULL) {
   
+  
+  ### prepare path, options, working directory, coloring
+  file <- normalizePath(file)
+  git.options <- paste("--color=always --color-words --word-diff", git.options)
+  
+  # set working directory so that git will work
+  wd <- getwd()
+  setwd(dirname(file))
+  
+  # save git colors and set to standard colors: 
+  color.diff <- data.frame(name = c("color.diff.plain", 
+                                    "color.diff.meta", 
+                                    "color.diff.frag", 
+                                    "color.diff.func", 
+                                    "color.diff.old", 
+                                    "color.diff.new", 
+                                    "color.diff.commit", 
+                                    "color.diff.whitespace"), 
+                           standard = c("", 
+                                        "bold", 
+                                        "cyan", 
+                                        "magenta", 
+                                        "red", 
+                                        "green", 
+                                        "yellow", 
+                                        "blue"), 
+                           pattern = c("(.*?)\\[m", 
+                                       "\033\\[1m(.+?)\\[m", 
+                                       "\033\\[36m(.+?)\\[m", 
+                                       "\033\\[35m(.+?)\\[m", 
+                                       "\033\\[31m(.+?)\\[m", 
+                                       "\033\\[32m(.+?)\\[m", 
+                                       "\033\\[33m(.+?)\\[m", 
+                                       "\033\\[34m(.+?)\\[m"),
+                           replacement = c("\\1", 
+                                           "<span class=\"meta\">\\1</span>",
+                                           "<span class=\"frag\">\\1</span>",
+                                           "<span class=\"func\">\\1</span>",
+                                           "<del>\\1</del>",
+                                           "<ins>\\1</ins>",
+                                           "<span class=\"commit\">\\1</span>",
+                                           "<span class=\"whitespace\">\\1</span>"),
+                           old = rep("", 8), 
+                           stringsAsFactors = FALSE)
+  
+  
+  
+  for (color.diff.name in color.diff[["name"]]) {
+    # save old coloring information
+    color <- suppressWarnings(system(paste("git config --global", color.diff.name), intern = TRUE))
+    if (!is.null(attr(color, "status"))) {
+      color <- ""
+    }
+    color.diff[color.diff[["name"]] == color.diff.name, "old"] <- color
+    
+    # set standard coloring
+    standard.color <- color.diff[color.diff[["name"]] == color.diff.name, "standard"]
+    if (standard.color == "") {
+      system(paste("git config --global --unset", color.diff.name))
+    } else {
+      system(paste("git config --global", color.diff.name, standard.color))
+    }
+  }
+
+  
+  # do stuff
+
+  
+  
+  ### restore coloring and working directory
+  # set back git colors
+  for (color.diff.name in color.diff[["name"]]) {
+    old.color <- color.diff[color.diff[["name"]] == color.diff.name, "old"]
+    if (color.diff.standard[[color.diff.name]] == "") {
+      system(paste("git config --global --unset", color.diff.name))
+    } else {
+      system(paste("git config --global", color.diff.name, old.color))
+    }
+  }
+  
+  #set back working directory
+  setwd(wd)
   
 }
 
@@ -23,10 +107,6 @@ show_diff <- function (
 # set git config diff colors to standard
 # git config --global color.diff.new "green"
 # color.diff
-# 
-# Whether to use ANSI escape sequences to add color to patches. If this is set to always, git-diff(1), git-log(1), and git-show(1) will use color for all patches. If it is set to true or auto, those commands will only use color when output is to the terminal. Defaults to false.
-# 
-# This does not affect git-format-patch(1) or the git-diff-* plumbing commands. Can be overridden on the command line with the --color[=<when>] option.
 # color.diff.<slot>
 #   
 #   Use customized color for diff colorization. <slot> specifies which part of the patch to use the specified color, and is one of plain (context text), meta (metainformation), frag (hunk header), func (function in hunk header), old (removed lines), new (added lines), commit (commit headers), or whitespace (highlighting whitespace errors). The values of these variables may be specified as in color.branch.<slot>.
